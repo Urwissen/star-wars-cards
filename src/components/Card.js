8770d6b316b1ec520960e096a869ee0c1d6fef3c
Lars <l.gessner@posteo.de>
2022-02-10 21:57:19 +0100

    add new api, include img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import person from "../boba.webp"
+
+const noPic = "https://c.tenor.com/en9QVrLjdzYAAAAC/star-wars-storm-trooper.gif"
 
 export const Card = (props) => {
   return (
     <div className='card'>
-        <img className="card--img" src={props.img ? props.img : person} alt="a person"/>
-        <div className='card--wrapper'>
-            <h2 className='card--title'>{props.name}</h2>
+        <img className="card--img" src={props.img ? props.img : noPic} alt="a person"/>
+        <h2 className='card--title'>{props.name}</h2>
+        <div className='card--wrapper'>  
             <ul className='card--list'>
-                <li className='card--list_item'>Größe: {props.height}</li>
-                <li className='card--list_item'>Spezies: xx</li>
-                <li className='card--list_item'>Planet: xx</li>
-                <li className='card--list_item'>Franktion: xxx</li>
+                <li className='card--list_item'><strong>Species:</strong><br /> {props.species}</li>
+                <li className='card--list_item'><strong>Homeworld:</strong><br /> {props.homeworld}</li>
+                {props.gender && <li className='card--list_item'><strong>Gender:</strong><br /> {props.gender}</li>}
+                {props.class && <li className='card--list_item'><strong>Class:</strong><br /> {props.class}</li>}
             </ul>
-            <p className='card--description'>Some content for the card</p>
+            {props.about && <a href={props.about}>about</a>}
         </div>
         
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,30 @@ import { Card } from './Card'
 export const Main = () => {
   const [data, setData] = React.useState([])
 
+  const url = "https://akabab.github.io/starwars-api/api/all.json"
+
   React.useEffect(() => {
-    fetch("https://swapi.dev/api/people/")
+    fetch(url)
     .then(response => response.json())
-    .then(data => setData(data.results))
+    .then(data => setData(data))
+    .catch(Error => console.log("Fetching problem: " + Error))
   }, [])
+
   
   console.log("render")
 
   return (
     <section className='cards--container'>
-      {data.map(person => <Card key={person.name} name={person.name}/>)}  
+      {data.map(person => <Card 
+        key={person.id} 
+        name={person.name} 
+        img={person.image} 
+        species={person.species} 
+        homeworld={person.homeworld} 
+        affiliations={person.affiliations} 
+        gender={person.gender ? person.gender : null}
+        class={person.class ? person.class : null}
+        about={person.wiki}/>)}  
     </section>
   )
 }
